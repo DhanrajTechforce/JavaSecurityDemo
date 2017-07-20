e5784fc228a7e9add6c6885b2926778b314188e3
dhanaarj <khodaliyad@gmail.com>
2017-07-20 10:08:04 +0530

    InitialStage

---
+++ Basic-Authentication/src/main/java/com/basic/authentication/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.basic.authentication;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("hello with security");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
